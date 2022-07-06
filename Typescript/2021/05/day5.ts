a05f5f49e948fd809abbd54a70d656c5d186aa33
marwwin <markus.kankkonen@protonmail.com>
2022-07-06 22:22:33 +0300

    solved day5 part 1

--- Typescript/2021/05/day5.ts
+++ Typescript/2021/05/day5.ts
@@ -1,37 +1,56 @@
-const solveDay5 = (input:string[]) => {
-  const seafloor = createSeafloor
+interface OceanFloor {
+  [key: string]: number;
 }
 
-const createSeafloor = (size: number): { [key: string]: number } => {
-  const seafloor: { [key: string]: number } = {};
-  for (let x: number = 0; x < size; x++) {
-    for (let y = 0; y < size; y++) {
-      seafloor[`${x}_${y}`] = 0;
-    }
+const solveDay5 = (input: string[]) => {
+  let seafloor: OceanFloor = {};
+  for (let line of input) {
+    seafloor = addLine(line, seafloor);
   }
-  return seafloor;
+  return Object.values(seafloor)
+    .filter((v) => v >= 2)
+    .length;
 };
 
-const addLine = (lineString: string, seafloor:  { [key: string]: number }) => {
+const addLine = (lineString: string, seafloor: OceanFloor) => {
   const line = parseLine(lineString);
   line.forEach(([x, y]) => {
-    const prev = seafloor[`${x}_${y}`];
+    const prev = seafloor[`${x}_${y}`] || 0;
     seafloor[`${x}_${y}`] = prev + 1;
   });
   return seafloor;
 };
 
-const parseLine = (line: string) => {
+const parseLine = (line: string): number[][] => {
   const [start, end] = line.split(" -> ");
-  let [startx, starty] = start.split(",").map((e) => parseInt(e));
+  const [startx, starty] = start.split(",").map((e) => parseInt(e));
   const [endx, endy] = end.split(",").map((e) => parseInt(e));
-  const result = [];
-  for (startx; startx <= endx; startx++) {
-    for (starty; starty <= endy; starty++) {
-      result.push([startx, starty]);
-    }
+  let result:number[][] = [];
+
+  if (startx !== endx && starty !== endy) {
+  } else if (startx === endx) {
+    
+    result = starty < endy ? parseVerticalLine(startx, starty, endy) : parseVerticalLine(startx, endy, starty);
+  } else if (starty === endy) {
+    result = startx < endx ? parseHorizontalLine(starty, startx, endx):parseHorizontalLine(starty, endx, startx) ;
   }
   return result;
 };
 
-export { createSeafloor, addLine, parseLine };
+const parseHorizontalLine = (y: number, start: number, end: number): number[][] => {
+  const result = [];
+  for (let i = start; i <= end; i++) {
+    result.push([i, y]);
+  }
+  return result;
+};
+
+const parseVerticalLine = (x: number, start: number, end: number): number[][] => {
+  const result = [];
+  for (let i = start; i <= end; i++) {
+    result.push([x, i]);
+  }
+  return result;
+};
+
+export { addLine, parseLine, solveDay5 };
